40d3ad053cba96920ee6e49c1de02bbd86e1edf3
Raghu Betina <raghu@firstdraft.com>
2019-05-29 19:47:55 +0000

    Whitelist columns in Leader for ActiveAdmin

--- app/admin/leaders.rb
+++ app/admin/leaders.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Leader do
+
+ permit_params :name, :description, :leader_ability, :leader_unit, :leader_agenda
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
