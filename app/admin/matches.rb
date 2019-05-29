019803eac140c740cea28000f365f72aaa869051
Raghu Betina <raghu@firstdraft.com>
2019-05-29 19:48:08 +0000

    Whitelist columns in Match for ActiveAdmin

--- app/admin/matches.rb
+++ app/admin/matches.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Match do
+
+ permit_params :map_id, :faction01_id, :faction02_id, :faction03_id, :faction04_id, :faction05_id, :faction06_id, :faction07_id, :faction08_id, :faction09_id, :faction10_id, :faction11_id, :faction12_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
