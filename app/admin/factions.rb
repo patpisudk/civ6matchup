0dfbb762f2bcd878c91dcf909b4e987b8f4930e4
Raghu Betina <raghu@firstdraft.com>
2019-05-29 19:47:58 +0000

    Whitelist columns in Faction for ActiveAdmin

--- app/admin/factions.rb
+++ app/admin/factions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Faction do
+
+ permit_params :civ_id, :leader_id, :dom_score_land, :dom_score_water, :dom_def_land, :dom_def_water, :sci_score_land, :sci_score_water, :sci_def_land, :sci_def_water, :cul_score_land, :cul_score_water, :cul_def_land, :cul_def_water, :rel_score_land, :rel_score_water, :rel_def_land, :rel_def_water, :dip_score_land, :dip_score_water, :dip_def_land, :dip_def_water
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
